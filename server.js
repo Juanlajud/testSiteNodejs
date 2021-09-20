a4a124b0be75db861b6621c94d99c5551d0a3ee1
Juan Lajud <juanlajud@gmail.com>
2021-09-20 11:36:07 -0300

    adding  app listening

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')    
 const app = express()    
-const port = 5000    
+const port = 3000    
 app.get('/', (req, res) => {    
-  res.send('Hello Jumbo world!')    
+  res.send('Hello World!')    
 })    
-app.listen(port);
+app.listen(port, () => {    
+  console.log(`Example app listening at http://localhost:${port}`)    
+})  
