454d8d70a33658604c98f1f67160460d26ee8699
Juan Lajud <juanlajud@gmail.com>
2021-09-22 16:04:04 -0300

    appinsights

--- main.tf
+++ main.tf
@@ -50,8 +50,33 @@ resource "azurerm_container_group" "jwacontainer" {
       protocol = "TCP"
     }
   }
+}
+
+resource "azurerm_application_insights" "appinsights" {
+  name                = "appinsights"
+  location            = azurerm_resource_group.JWA.location
+  resource_group_name = azurerm_resource_group.JWA.name
+  application_type    = "web"
 
 }
+
+/*
+resource "azurerm_log_analytics_workspace" "example" {
+  name                = "workspace-test"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_application_insights" "example" {
+  name                = "tf-test-appinsights"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  workspace_id        = azurerm_log_analytics_workspace.example.id
+  application_type    = "web"
+}
+*/
 
 resource "azurerm_servicebus_namespace" "jumbolajudapp" {
   name                = "jumbolajudapp"
